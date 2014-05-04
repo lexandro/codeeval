f40d3b716d53c66b4e7fce83642281048569be36
lexandro <norton>
2014-05-04 11:10:39 +0200

    RightMost challenge solved

---
+++ RightmostChar/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // setup input
+        BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+        String fileLine;
+        //
+        while ((fileLine = reader.readLine()) != null) {
+            String[] items = fileLine.split(",");
+            System.out.println(items[0].lastIndexOf(items[1]));
+        }
+    }
+
+
+}
