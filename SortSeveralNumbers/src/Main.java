397ab1e03e38971ecd4ae8b19e3abf0fb1bf557a
lexandro <norton>
2014-05-06 20:08:45 +0200

    Sort Several Numbers challenge solved

---
+++ SortSeveralNumbers/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        solveChallenge(args);
+    }
+
+    private static void solveChallenge(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+        String fileLine;
+        //
+        while ((fileLine = reader.readLine()) != null) {
+            System.out.println(sortSeveralNumbers(fileLine));
+        }
+    }
+
+    private static String sortSeveralNumbers(String fileLine) {
+        String[] numberStrings = fileLine.split(" ");
+        BigDecimal[] numbers = new BigDecimal[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            numbers[i] = new BigDecimal(numberStrings[i]);
+        }
+        Arrays.sort(numbers);
+        StringBuilder result = new StringBuilder(numbers[0].toString());
+        for (int i = 1; i < numbers.length; i++) {
+            result.append(" ").append(numbers[i].toString());
+        }
+        return result.toString();
+    }
+}
