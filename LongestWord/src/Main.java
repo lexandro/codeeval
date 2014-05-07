81b443a3bd25e69d7d5f1c9db6abd41bc25f7d30
lexandro <lexandro2000@gmail.com>
2014-05-07 17:06:01 +0200

    Longest Word challenge solved

---
+++ LongestWord/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+  Author: Robert Stern (lexandro2000@gmail.com)
+  All of my solutions are available from https://github.com/lexandro/codeeval
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        solveChallenge(args);
+    }
+
+    private static void solveChallenge(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+        String fileLine;
+        //
+        while ((fileLine = reader.readLine()) != null) {
+            System.out.println(longestWord(fileLine));
+        }
+    }
+
+    private static String longestWord(String fileLine) {
+        String[] words = fileLine.split(" ");
+        int maxSize = 0;
+        String result = null;
+        for (String word : words) {
+            if (word.length() > maxSize) {
+                maxSize = word.length();
+                result = word;
+            }
+        }
+        return result;
+    }
+}
+
