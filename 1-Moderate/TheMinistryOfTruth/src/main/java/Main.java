dd765d35cf5fb3091f629054837669129e7f1673
lexandro <norton>
2014-06-29 21:42:38 +0200

    Ministry of Truth moderate challenge solved

--- 1-Moderate/TheMinistryOfTruth/src/main/java/Main.java
+++ 1-Moderate/TheMinistryOfTruth/src/main/java/Main.java
@@ -8,12 +8,10 @@ import java.io.FileReader;
  */
 public class Main {
 
+    public static final char[] EMPTY = new char[0];
+
     public static void main(String[] args) throws Throwable {
-//        solveChallenge(args);
-        theMinistryOfTruth("Higher meaning;Hi mean");
-        theMinistryOfTruth("this is impossible;im possible");
-        theMinistryOfTruth("twenty   two minutes;two minutes");
-        theMinistryOfTruth("Higher meaning;e");
+        solveChallenge(args);
     }
 
     private static void solveChallenge(String[] args) throws Throwable {
@@ -28,7 +26,45 @@ public class Main {
     }
 
     private static void theMinistryOfTruth(String fileLine) {
+        String[] items = fileLine.split(";");
+        String[] originalWords = items[0].split("\\s+");
+        String[] newWords = items.length == 2 ? items[1].split(" ") : new String[0];
+        // reserve enough space to prevent reallocation
+        StringBuilder result = new StringBuilder(items[0].length());
+        //
+        int index = 0;
+        for (String originalWord : originalWords) {
+            if (index >= newWords.length) {
+                result.append(pad(originalWord.length()));
+            } else {
+                int pos = originalWord.indexOf(newWords[index]);
+                if (pos >= 0) {
+                    result.append(pad(pos));
+                    result.append(newWords[index]);
+                    result.append(pad(originalWord.length() - newWords[index].length() - pos));
+                    index++;
+                    //
+                } else {
+                    result.append(pad(originalWord.length()));
+                }
+            }
+            result.append(' ');
 
+        }
+        System.out.println((index == newWords.length ? result : "I cannot fix history"));
+    }
 
+    private static char[] pad(int size) {
+        char[] result;
+        if (size > 0) {
+            result = new char[size];
+        } else {
+            return EMPTY;
+        }
+        //
+        for (int i = 0; i < size; i++) {
+            result[i] = '_';
+        }
+        return result;
     }
 }
