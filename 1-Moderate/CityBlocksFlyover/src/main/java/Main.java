0212853f2023394903e8676103bc354fe5bba59d
lexandro <norton>
2014-08-04 23:02:11 +0200

    City Blocks Flyover moderate challenge solved

--- 1-Moderate/CityBlocksFlyover/src/main/java/Main.java
+++ 1-Moderate/CityBlocksFlyover/src/main/java/Main.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.regex.Pattern;
+
+import static java.lang.Math.*;
 
 /*
   Author: Robert Stern (lexandro2000@gmail.com)
@@ -10,14 +10,8 @@ import java.util.regex.Pattern;
  */
 public class Main {
 
-    private static final Pattern DECIMAL_PATTERN = Pattern.compile("\\d+");
-
     public static void main(String[] args) throws Throwable {
-//        solveChallenge(args);
-        cityBlocksFlyover("(0,2,4,8,10,13,14,18,22,23,24,33,40,42,44,47,49,53,55,63,66,81,87,91)");
-        cityBlocksFlyover("(0,147,220)");
-        cityBlocksFlyover("(0,1,2,4) (0,1,3,4,5)");
-        cityBlocksFlyover("(0,1,3,4,6) (0,1,2,4)");
+        solveChallenge(args);
     }
 
     private static void solveChallenge(String[] args) throws Throwable {
@@ -33,25 +27,33 @@ public class Main {
 
     private static void cityBlocksFlyover(String fileLine) {
         String[] items = fileLine.split(" ");
-        int[] streets = convertStringToIntArray(items[0]);
-        int[] avenues = items.length > 1 ? convertStringToIntArray(items[1]) : new int[]{1};
+        double[] streets = convertStringToIntArray(items[0]);
+        double[] avenues = convertStringToIntArray(items[1]);
         //
-        float helicopterRatio = Float.valueOf(avenues[avenues.length - 1]) / streets[streets.length - 1];
+        double helicopterRatio = avenues[avenues.length - 1] / streets[streets.length - 1];
         //
-        for (int streetIndex = 0; streetIndex < streets.length - 1; streetIndex++) {
-            for (int avenueIndex = 0; avenueIndex < avenues.length - 1; avenueIndex++) {
-
+        int count = 0;
+        for (int x = 0; x < streets.length - 1; x++) {
+            for (int y = 0; y < avenues.length - 1; y++) {
+                if (isBetween(streets[x], streets[x + 1], avenues[y] / helicopterRatio, avenues[y + 1] / helicopterRatio)) {
+                    count++;
+                }
             }
         }
+        System.out.println(count);
     }
 
-    private static int[] convertStringToIntArray(String numberList) {
+    private static boolean isBetween(double x1, double x2, double y1, double y2) {
+        return max(x1, y1) < min(x2, y2);
+    }
+
+    private static double[] convertStringToIntArray(String numberList) {
         numberList = numberList.replaceAll("[(|)]", "");
         String[] numberStrings = numberList.split(",");
-        int[] result = new int[numberStrings.length];
+        double[] result = new double[numberStrings.length];
         //
         for (int i = 0; i < result.length; i++) {
-            result[i] = Integer.parseInt(numberStrings[i], 10);
+            result[i] = Double.valueOf(numberStrings[i]);
         }
         return result;
     }
