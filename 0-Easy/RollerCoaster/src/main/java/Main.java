66292e3d0bb6d4449e3690f4cc7cff9da74e84f6
lexandro <norton>
2014-08-09 00:01:34 +0200

    Roller Coaster easy challenge solved in java and python

--- 0-Easy/RollerCoaster/src/main/java/Main.java
+++ 0-Easy/RollerCoaster/src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 /*
   Author: Robert Stern (lexandro2000@gmail.com)
