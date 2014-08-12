e9b175c6a947c8d02a5bc7ab0b0a8a12b38da43b
lexandro <norton>
2014-08-12 23:57:50 +0200

    Multiplication tables easy challenge solved in python

---
+++ python/0-Easy/MultiplicationTables/MultiplicationTables.py
@@ -0,0 +1,10 @@
+"""
+Author: Robert Stern (lexandro2000@gmail.com)
+All of my solutions are available from https://github.com/lexandro/codeeval
+"""
+
+for i in xrange(1, 13):
+  for j in xrange(1, 13):
+    print  str(i * j).rjust(3),
+  print
+
