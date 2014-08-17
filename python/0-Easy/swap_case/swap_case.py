2d36d21d857cd8b05626ef2bd6dcc979b4936b0e
lexandro <norton>
2014-08-18 00:54:43 +0200

    Swap Case easy challenge solved in python

---
+++ python/0-Easy/swap_case/swap_case.py
@@ -0,0 +1,12 @@
+import sys
+
+"""
+Author: Robert Stern (lexandro2000@gmail.com)
+All of my solutions are available from https://github.com/lexandro/codeeval
+"""
+
+test_cases = open(sys.argv[1], 'r')
+
+for test in test_cases:
+  print test.strip().swapcase()
+test_cases.close()
