3084c02c967dfce636eefa48b87a5a7c12cd08da
lexandro <norton>
2014-08-17 23:45:08 +0200

    Simple Sorting easy challenge solved in python

--- python/0-Easy/simple_sorting/simple_sorting.py
+++ python/0-Easy/simple_sorting/simple_sorting.py
@@ -6,12 +6,12 @@ All of my solutions are available from https://github.com/lexandro/codeeval
 """
 
 
-def number_sorting(file_line):
+def simple_sorting(file_line):
   print " ".join([('{:.3f}'.format(num)) for num in sorted(float(i) for i in file_line.split(' '))])
 
 
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
   test = test.strip()
-  number_sorting(test)
+  simple_sorting(test)
 test_cases.close()
