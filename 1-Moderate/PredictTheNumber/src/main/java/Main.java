f2778b0ff392620195d765920ca9605c235088cb
lexandro <norton>
2014-10-11 13:40:36 +0200

    Predict the number moderate challenge solved in java

---
+++ 1-Moderate/PredictTheNumber/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/*
+  Author: Robert Stern (lexandro2000@gmail.com)
+  All of my solutions are available from https://github.com/lexandro/codeeval
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws Throwable {
+        solveChallenge(args);
+    }
+
+    private static void solveChallenge(String[] args) throws Throwable {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+        String fileLine;
+        //
+        while ((fileLine = reader.readLine()) != null) {
+            if (!fileLine.isEmpty()) {
+                predictTheNumber(fileLine);
+            }
+        }
+    }
+
+    private static void predictTheNumber(String fileLine) {
+        System.out.println(Long.toBinaryString(Long.valueOf(fileLine)).replaceAll("0", "").length() % 3);
+    }
+}
