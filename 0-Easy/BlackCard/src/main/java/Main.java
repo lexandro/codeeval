93ff3cadcde8b55b54b8fff4484bd2133e3d5529
lexandro <lexandro2000@gmail.com>
2016-02-13 01:33:03 +0100

    Solved BlackCard easy challenge

---
+++ 0-Easy/BlackCard/src/main/java/Main.java
@@ -0,0 +1,54 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+  Author: Robert Stern (lexandro2000@gmail.com)
+  All of my solutions are available from https://github.com/lexandro/codeeval
+ */
+public class Main {
+
+    public static final int MAX_TEAMS = 7;
+
+    public static void main(String[] args) throws Throwable {
+        solveChallenge(args);
+    }
+
+    private static void solveChallenge(String[] args) throws Throwable {
+        Files.lines(Paths.get(args[0])).forEach(Main::blackCard);
+    }
+
+    protected static void blackCard(String fileLine) {
+        String[] elements = fileLine.split("\\|");
+        int steps = Integer.parseInt(elements[1].trim());
+        String[] players = elements[0].split(" ");
+        //
+        List<String> playersList = convertPlayerArrayToLinkedList(players);
+        //
+        while (playersList.size() > 1) {
+            Iterator<String> playersIterator = playersList.iterator();
+            for (int count = 0; count < steps; count++) {
+                if (!playersIterator.hasNext()) {
+                    playersIterator = playersList.iterator();
+                }
+                playersIterator.next();
+            }
+            playersIterator.remove();
+        }
+        System.out.println(playersList.get(0));
+
+    }
+
+    private static List<String> convertPlayerArrayToLinkedList(String[] players) {
+        List<String> playersList = new LinkedList<>();
+        for (String player : players) {
+            playersList.add(player);
+        }
+        return playersList;
+    }
+
+}
